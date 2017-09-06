8ee20ab9ddcc163c2acddfcf8e1392f30cc36cfb
Bviews <bviewscn@gmail.com>
2017-09-06 21:07:36 +0800

    change AWS S3 to QINIU

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -90,26 +90,18 @@ INSTALLED_APPS += ['gunicorn', ]
 # ------------------------------------------------------------------------------
 # Uploaded Media Files
 # ------------------------
-# See: http://django-storages.readthedocs.io/en/latest/index.html
-INSTALLED_APPS += ['storages', ]
+# See: http://django-qiniu-storage.readthedocs.org/zh_CN/latest/
 
-AWS_ACCESS_KEY_ID = env('DJANGO_AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('DJANGO_AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('DJANGO_AWS_STORAGE_BUCKET_NAME')
-AWS_AUTO_CREATE_BUCKET = True
-AWS_QUERYSTRING_AUTH = False
-AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
+QINIU_ACCESS_KEY = env('DJANGO_QINIU_ACCESS_KEY')
+QINIU_SECRET_KEY = env('DJANGO_QINIU_SECRET_KEY')
+QINIU_BUCKET_NAME = env('DJANGO_QINIU_BUCKET_NAME')
+QINIU_BUCKET_DOMAIN = env('DJANGO_QINIU_BUCKET_DOMAIN')
+QINIU_SECURE_URL = env.bool('DJANGO_QINIU_SECURE_URL', default=False)
 
-# AWS cache settings, don't change unless you know what you're doing:
-AWS_EXPIRY = 60 * 60 * 24 * 7
-
-# TODO See: https://github.com/jschneier/django-storages/issues/47
-# Revert the following and use str after the above-mentioned bug is fixed in
-# either django-storage-redux or boto
-control = 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY, AWS_EXPIRY)
-AWS_HEADERS = {
-    'Cache-Control': bytes(control, encoding='latin-1')
-}
+if QINIU_SECURE_URL:
+    QINIU_PREFIX = 'https://'
+else:
+    QINIU_PREFIX = 'http://'
 
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
